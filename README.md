ee69f95ffd612347c369442de52325a083a9eeaa
Kenji Phang <ken.pcx@outlook.com>
2024-01-14 17:13:11 +0000

    Added instructions for playing generated mocaps

--- README.md
+++ README.md
@@ -15,5 +15,17 @@ Setup the conda environment with the following command:
 conda env create -f environment.yml
 ```
 
+### Running the main motion generation code
+
 Now you are able to run the notebook, the only one to pay attention to is `temporal_unet_diffusion.ipynb`. I have left headers and comments in the notebook to explain what each cell does.
+
+### Playing the generated motions
+
+I have included a script to play the generated motions. To use it, run the following command:
+
+```bash
+python3 mocap_player.py logs/{exp_name}/sampled_motions/motion1.npy
+
+eg: python3 mocap_player.py logs/walk-motion/sampled_motions/motion1.npy
+```
 
--- diffusion/mocap_player.py
+++ diffusion/mocap_player.py
@@ -4,7 +4,7 @@ import numpy as np
 def play_with_pos_vel(mocap_filepath):
     from mujoco_py import load_model_from_xml, MjSim, MjViewer
 
-    xmlpath = "diffusion/assets/dp_env_v2.xml"
+    xmlpath = "assets/dp_env_v2.xml"
     with open(xmlpath) as fin:
         MODEL_XML = fin.read()
 
@@ -44,54 +44,11 @@ def play_with_pos_vel(mocap_filepath):
     # vid_save.close()
 
 
-def play_with_frame_data(mocap_filepath):
-    from mujoco_py import load_model_from_xml, MjSim, MjViewer
-    from diffusion.utils.mocap_v2 import MocapDM
-
-    xmlpath = "/home/kenji/Fyp/DeepMimic_mujoco/diffusion/mujoco/humanoid_deepmimic/envs/asset/dp_env_v2.xml"
-    with open(xmlpath) as fin:
-        MODEL_XML = fin.read()
-
-    model = load_model_from_xml(MODEL_XML)
-    sim = MjSim(model)
-    viewer = MjViewer(sim)
-
-    # Load the mocap
-    mocap = np.load(mocap_filepath)
-    mocap_dm = MocapDM()
-    mocap_dm.load_mocap_from_raw(mocap)
-    from time import sleep
-
-    phase_offset = np.array([0.0, 0.0, 0.0])
-
-    data_config = mocap_dm.data_config
-    data_vel = mocap_dm.data_vel
-
-    # import cv2
-    # from VideoSaver import VideoSaver
-    # width = 640
-    # height = 480
-
-    # vid_save = VideoSaver(width=width, height=height)
-    while True:
-        # for i in range(5):
-        for config, vel in zip(data_config, data_vel):
-            tmp_val = config
-            sim_state = sim.get_state()
-            sim_state.qpos[:] = tmp_val[:]
-            sim_state.qpos[:3] += phase_offset[:]
-            sim_state.qvel[:] = vel[:]
-            sim.set_state(sim_state)
-            sim.forward()
-            viewer.render()
-            # vid_save.addFrame(viewer.read_pixels(width, height, depth=False))
-
-        sim_state = sim.get_state()
-        phase_offset = sim_state.qpos[:3]
-        phase_offset[2] = 0
-
-    # vid_save.close()
-
+import argparse
 
 if __name__ == "__main__":
-    play_with_pos_vel("diffusion/logs/walk-motion2/sampled_motions/motion1.npy")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("motion_file", help="Path to the motion file")
+    args = parser.parse_args()
+
+    play_with_pos_vel(args.motion_file)
